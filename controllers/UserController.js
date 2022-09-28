0ed9b73ed92d764b14e914a6927ab008f8555db4
Diogo Lima <diogoslva0133@gmail.com>
2022-09-28 10:10:58 -0300

    deleting user data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -216,7 +216,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delet btn-xs btn-flat">Excluir</button>
             </td>
       `;
 
@@ -228,6 +228,18 @@ class UserController {
     }
 
     addEventsTr(tr){
+
+        tr.querySelector(".btn-delet").addEventListener("click", e => {
+
+            if (confirm("Deseja realmente excluir?")) {
+               
+                tr.remove();
+
+                this.updateCount();
+
+            }    
+        });
+
 
         tr.querySelector(".btn-edit").addEventListener("click", e =>{
 
